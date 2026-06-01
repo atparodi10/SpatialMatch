b95feed61e82b90bfe7c8cd5603364420a6ed85a
atparodi10 <joatorres@uamv.edu.ni>
2026-06-01 17:21:36 -0600

    feat: inicialización de la estructura base del proyecto.

---
+++ src/main/java/ni/edu/uam/SpatialMatch/run/SpatialMatch.java
@@ -0,0 +1,16 @@
+package ni.edu.uam.SpatialMatch.run;
+
+import org.openxava.util.*;
+
+/**
+ * Ejecuta esta clase para arrancar la aplicación.
+ */
+
+public class SpatialMatch {
+
+	public static void main(String[] args) throws Exception {
+		DBServer.start("SpatialMatch-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		AppServer.run("SpatialMatch"); // Usa AppServer.run("") para funcionar en el contexto raíz
+	}
+
+}
