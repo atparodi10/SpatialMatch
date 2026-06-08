64434f0f2342b2b5701c2cb69b1b30a9d59663ca
atparodi10 <joatorres@uamv.edu.ni>
2026-06-08 16:14:15 -0600

    feat: implementar atributos, mapeo JPA y anotaciones en Evaluacion

--- src/main/java/ni/edu/uam/SpatialMatch/modelo/Evaluacion.java
+++ src/main/java/ni/edu/uam/SpatialMatch/modelo/Evaluacion.java
@@ -1,4 +1,46 @@
 package ni.edu.uam.SpatialMatch.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import org.openxava.calculators.CurrentLocalDateCalculator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
 public class Evaluacion {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @Hidden
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(length = 36)
+    private String oid;
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    private LocalDate fechaCreacion;
+
+    @Column
+    private int puntajeFinal;
+
+    @Column
+    private long tiempoTotalPrueba;
+
+    @Column(columnDefinition = "TEXT")
+    @Stereotype("MEMO")
+    private String retroalimentacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @DescriptionsList
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "evaluacion", cascade = CascadeType.ALL)
+    private Collection<RegistroRespuesta> registroRespuestas;
+
 }
