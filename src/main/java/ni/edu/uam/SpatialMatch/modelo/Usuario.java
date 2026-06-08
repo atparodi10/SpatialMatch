05f10fc0769e025458b302b93b258de8d514f16c
atparodi10 <joatorres@uamv.edu.ni>
2026-06-08 15:59:54 -0600

    feat: implementar atributos, mapeo JPA y anotaciones en Usuario.

--- src/main/java/ni/edu/uam/SpatialMatch/modelo/Usuario.java
+++ src/main/java/ni/edu/uam/SpatialMatch/modelo/Usuario.java
@@ -2,22 +2,46 @@ package ni.edu.uam.SpatialMatch.modelo;
 
 import java.math.*;
 import java.time.*;
+import java.util.Collection;
 
 import javax.persistence.*;
 
+import org.hibernate.annotations.GenericGenerator;
 import org.openxava.annotations.*;
 import org.openxava.model.*;
 
 import lombok.*;
 
 @Entity @Getter @Setter
-public class Usuario extends Identifiable {
-	
-	@Column(length=50) @Required
-	String descripcion;
-	
-	LocalDate fecha;
-	
-	BigDecimal importe;
+public class Usuario {
+
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@Hidden
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
+	@Column(length = 36)
+	private String oid;
+
+	@Column(length = 50)
+	@Required
+	private String nombre;
+
+	@Column(length = 50)
+	@Required
+	private String apellido;
+
+	@Column(length = 50)
+	@Required
+	@Stereotype("EMAIL")
+	private String correo;
+
+	@Column(length = 50)
+	@Required
+	private String rol;
+
+	@OneToMany(mappedBy = "usuario")
+	@ListProperties(fechaCreacion, puntajeFinal, tiempoTotalPrueba)
+	private Collection<Evaluacion> evaluaciones;
+
 	
 }
