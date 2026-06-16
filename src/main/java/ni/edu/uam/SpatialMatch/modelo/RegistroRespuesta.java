a9b96ac3c12a678d89f0a95f19d252c582833b77
atparodi10 <joatorres@uamv.edu.ni>
2026-06-16 17:12:34 -0600

    fix: cambiar el tipo de dato opcionSeleccionada a Enum para resolver el error  de mapeo de JPA

--- src/main/java/ni/edu/uam/SpatialMatch/modelo/RegistroRespuesta.java
+++ src/main/java/ni/edu/uam/SpatialMatch/modelo/RegistroRespuesta.java
@@ -1,4 +1,76 @@
 package ni.edu.uam.SpatialMatch.modelo;
 
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Entity
+@Getter
+@Setter
+@View(
+        name = "PantallaPrueba",
+        members =
+                "Informacion del Ejercicio [" +
+                        "   pregunta.numeroPregunta;" +
+                        "   pregunta.imagenMatriz;" +
+                        "];" +
+                        "Tu Respuesta [" +
+                        "   opcionSeleccionada" +
+                        "]"
+)
 public class RegistroRespuesta {
+
+    public enum OpcionValida {
+        A, B, C, D, E
+    }
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @Hidden
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(length = 36)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 5)
+    @Required(message = "Debes seleccionar una opción antes de continuar")
+    private OpcionValida opcionSeleccionada;
+
+    @Column
+    private boolean esCorrecta;
+
+    @Column
+    private LocalDateTime horaInicio;
+
+    @Column
+    private LocalDateTime horaRespuesta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Evaluacion evaluacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NoCreate @NoModify @NoSearch
+    private Pregunta pregunta;
+
+    @PrePersist
+    @PreUpdate
+    private void evaluarRespuestaAutomatica() {
+        if (this.pregunta != null && this.opcionSeleccionada != null) {
+            this.esCorrecta = this.pregunta.validarRespuesta(this.opcionSeleccionada.name());
+        } else {
+            this.esCorrecta = false;
+        }
+    }
+
+    public long calcularSegundosInvertidos() {
+        if (this.horaInicio != null && this.horaRespuesta != null) {
+            return ChronoUnit.SECONDS.between(this.horaInicio, this.horaRespuesta);
+        }
+        return 0;
+    }
 }
