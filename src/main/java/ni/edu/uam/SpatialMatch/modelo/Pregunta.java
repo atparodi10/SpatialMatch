1de418649b2cb8bf0ba8350c0fc4a4531aaa1708
meringe4444 <ghostform13.st@gmail.com>
2026-06-22 23:19:18 -0600

    Configura vistas de pregunta para administracion y prueba

--- src/main/java/ni/edu/uam/SpatialMatch/modelo/Pregunta.java
+++ src/main/java/ni/edu/uam/SpatialMatch/modelo/Pregunta.java
@@ -1,4 +1,43 @@
 package ni.edu.uam.SpatialMatch.modelo;
 
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+@Entity
+@Getter
+@Setter
+@Views({
+        @View(
+                members =
+                        "numeroPregunta;" +
+                                "identificadorFigura;" +
+                                "imagenFigura;" +
+                                "opciones [#" +
+                                "   imagenOpcionA, imagenOpcionB, imagenOpcionC;" +
+                                "   imagenOpcionD" +
+                                "];" +
+                                "respuestaCorrecta"
+        ),
+        @View(
+                name = "ParaPrueba",
+                members =
+                        "numeroPregunta;" +
+                                "identificadorFigura;" +
+                                "imagenFigura;" +
+                                "opciones [#" +
+                                "   imagenOpcionA, imagenOpcionB, imagenOpcionC;" +
+                                "   imagenOpcionD" +
+                                "]"
+        )
+})
 public class Pregunta {
-}
