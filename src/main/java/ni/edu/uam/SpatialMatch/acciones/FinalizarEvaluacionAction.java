934d84da107f773fc80d262c377ea5af5a59f586
Koganih <gizamora@uamv.edu.ni>
2026-06-23 23:14:02 -0600

    Crea accion para finalizar evaluacion

---
+++ src/main/java/ni/edu/uam/SpatialMatch/acciones/FinalizarEvaluacionAction.java
@@ -0,0 +1,18 @@
+package ni.edu.uam.SpatialMatch.acciones;
+
+import org.openxava.actions.ViewBaseAction;
+import org.openxava.jpa.XPersistence;
+import ni.edu.uam.SpatialMatch.modelo.Evaluacion;
+
+import java.util.Date;
+
+public class FinalizarEvaluacionAction extends ViewBaseAction {
+
+    private static final String EVALUACION_ACTIVA_ID = "evaluacionActivaId";
+
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
+
