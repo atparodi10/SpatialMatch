e2897694bb27e91f87b1b3acd796fedda64733f0
meringe4444 <ghostform13.st@gmail.com>
2026-06-24 13:53:16 -0600

    Traslado a Main - Ver mas detalles en ramas de los diferentes desarrolladores, para visualizar creacion, cambios, ediciones, etc.

--- src/main/java/ni/edu/uam/SpatialMatch/modelo/RegistroRespuesta.java
+++ src/main/java/ni/edu/uam/SpatialMatch/modelo/RegistroRespuesta.java
@@ -1,4 +1,89 @@
 package ni.edu.uam.SpatialMatch.modelo;
 
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@View(
+        name = "PantallaPrueba",
+        members =
+                "Informacion del Ejercicio [" +
+                        "   pregunta;" +
+                        "];" +
+                        "Tu Respuesta [" +
+                        "   opcionSeleccionada" +
+                        "]"
+)
 public class RegistroRespuesta {
+
+    public enum OpcionValida {
+        A, B, C, D
+    }
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @Hidden
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(length = 36)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 5)
+    @Required(message = "Debes seleccionar una opcion antes de continuar")
+    @Editor(forViews = "PantallaPrueba", value = "ValidValuesRadioButton")
+    private OpcionValida opcionSeleccionada;
+
+    @Column
+    @ReadOnly
+    private boolean esCorrecta;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    @ReadOnly
+    @Stereotype("DATETIME")
+    private Date horaInicio;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    @ReadOnly
+    @Stereotype("DATETIME")
+    private Date horaRespuesta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NoCreate @NoModify @NoSearch
+    private Evaluacion evaluacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ReadOnly(forViews = "PantallaPrueba")
+    @ReferenceView(value = "ParaPrueba", forViews = "PantallaPrueba")
+    @NoCreate @NoModify @NoSearch
+    private Pregunta pregunta;
+
+    @PrePersist
+    @PreUpdate
+    private void evaluarRespuestaAutomatica() {
+        if (this.pregunta != null && this.opcionSeleccionada != null) {
+            this.esCorrecta = this.pregunta.validarRespuesta(this.opcionSeleccionada.name());
+        } else {
+            this.esCorrecta = false;
+        }
+    }
+
+    public long calcularSegundosInvertidos() {
+        if (this.horaInicio != null && this.horaRespuesta != null) {
+            return (this.horaRespuesta.getTime() - this.horaInicio.getTime()) / 1000;
+        }
+        return 0;
+    }
+
+    public String getResultadoCorreccion() {
+        return this.esCorrecta ? "Es correcta" : "No es correcta";
+    }
 }
